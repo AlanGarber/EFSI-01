89178de4e6a4354279fcf8468d8e6a3240b4570c
ALAN DAVID <46442525@ort>
2022-03-28 08:19:41 -0300

    Seventh commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const http = require("http");
 
-const host = 'localost';
-const port= 8000;
+const host = 'localhost';
+const port = 8000;
 
-const requestListener = function (req, res) {
-    res.writeHead(200);
-    res.end("My first server!");
+const requestListener = function (req, rest){
+    rest.writeHead(200);
+    rest.end("My first server");
 };
 
 const server = http.createServer(requestListener);

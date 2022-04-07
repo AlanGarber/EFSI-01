5467567fbf5ced21aa863a6ac40afb2da6ab5904
ALAN DAVID <46442525@ort>
2022-04-07 11:52:51 -0300

    succes

---
+++ EjerciciosEnClase/RaizArrayAnterior.js
@@ -0,0 +1,14 @@
+let arrayOriginal=[2,3,5,12,54,5,-1,0,23,66,7];
+let arrayDuplicado=arrayOriginal.concat(arrayOriginal);
+let arrayFinal=[];
+
+for(let i=0;i<arrayDuplicado.length;i++){
+    let obj={
+        orig:arrayDuplicado[i],
+        mod:Math.sqrt(arrayDuplicado[i])
+    }
+    arrayFinal.push(obj);
+}
+console.log(arrayFinal);
+
+

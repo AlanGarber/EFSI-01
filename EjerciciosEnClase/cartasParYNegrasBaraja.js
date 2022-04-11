bf5713ff215f775124cda23c6d4d9abe0e7c9ba2
ALAN DAVID <46442525@ort>
2022-04-11 10:51:00 -0300

    Finalizado

--- EjerciciosEnClase/cartasParYNegrasBaraja.js
+++ EjerciciosEnClase/cartasParYNegrasBaraja.js
@@ -2,6 +2,7 @@ let numerosPoker=["A",2,3,4,5,6,7,8,9,10,"J","Q","K"];
 let palosPoker=["Diamante","Corazon","Pica","Trebol"];
 let coloresPoker=["Rojo","Negro"];
 let baraja=[];
+let contadorNegrasPar=0;
 
 for(let i=0;i<palosPoker.length;i++){
     for(j=0;j<numerosPoker.length;j++){ 
@@ -11,6 +12,10 @@ for(let i=0;i<palosPoker.length;i++){
             
         }
         baraja.push(carta);
+        if((carta.palo=="Pica"||carta.palo=="Trebol") && (carta.numero%2==0)){
+            contadorNegrasPar++;
+        }
     }
 }
-console.log(baraja);
+console.log(contadorNegrasPar);
+ 
